98c6c18c3bcbbced02dcf29e5b31d6a071651e0d
winnie0710 <100845994+winnie0710@users.noreply.github.com>
2022-05-05 22:36:14 +0800

    Add files via upload

--- wifiserver_gpio.ino
+++ wifiserver_gpio.ino
@@ -10,8 +10,8 @@
 #include <WiFi.h>
 
 // Replace with your network credentials
-const char* ssid = "Your_SSID";
-const char* password = "Your_Password";
+const char* ssid = "OPPO Reno2 Z";
+const char* password = "winnie0710";
 
 // Set web server port number to 80
 WiFiServer server(80);
@@ -38,11 +38,9 @@ void setup() {
   Serial.begin(115200);
   // Initialize the output variables as outputs
   pinMode(output2, OUTPUT);
- 
+
   // Set outputs to LOW
   digitalWrite(output2, LOW);
- 
-
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -85,22 +83,14 @@ void loop(){
             client.println();
             
             // turns the GPIOs on and off
-            if (header.indexOf("GET /26/on") >= 0) {
+            if (header.indexOf("GET /2/on") >= 0) {
               Serial.println("GPIO 2 on");
-              output26State = "on";
+              output2State = "on";
               digitalWrite(output2, HIGH);
-            } else if (header.indexOf("GET /26/off") >= 0) {
+            } else if (header.indexOf("GET /2/off") >= 0) {
               Serial.println("GPIO 2 off");
-              output26State = "off";
+              output2State = "off";
               digitalWrite(output2, LOW);
-            } else if (header.indexOf("GET /27/on") >= 0) {
-              Serial.println("GPIO 27 on");
-              output27State = "on";
-              digitalWrite(output27, HIGH);
-            } else if (header.indexOf("GET /27/off") >= 0) {
-              Serial.println("GPIO 27 off");
-              output27State = "off";
-              digitalWrite(output27, LOW);
             }
             
             // Display the HTML web page
@@ -117,23 +107,16 @@ void loop(){
             // Web Page Heading
             client.println("<body><h1>ESP32 Web Server</h1>");
             
-            // Display current state, and ON/OFF buttons for GPIO 26  
-            client.println("<p>GPIO 26 - State " + output26State + "</p>");
-            // If the output26State is off, it displays the ON button       
-            if (output26State=="off") {
-              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            // Display current state, and ON/OFF buttons for GPIO 2  
+            client.println("<p>GPIO 2 - State " + output2State + "</p>");
+            // If the output2State is off, it displays the ON button       
+            if (output2State=="off") {
+              client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
             } else {
-              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+              client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
             } 
                
-            // Display current state, and ON/OFF buttons for GPIO 27  
-            client.println("<p>GPIO 27 - State " + output27State + "</p>");
-            // If the output27State is off, it displays the ON button       
-            if (output27State=="off") {
-              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
+            
             client.println("</body></html>");
             
             // The HTTP response ends with another blank line
